70e40bbe3f2ee686da23d73882fcd7db6776689f
Rajesh Timilsina <rajeshtims@gmail.com>
2024-04-19 13:42:26 -0400

    Added layout files for two fragments: Filter and Product Details. They are yet to be connected to main activity.

---
+++ Sustainify/app/src/main/java/com/example/sustainify/ProductDetailFragment.kt
@@ -0,0 +1,46 @@
+package com.example.sustainify
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProductDetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProductDetailFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_detail, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ProductDetailFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = ProductDetailFragment()
+    }
+}
